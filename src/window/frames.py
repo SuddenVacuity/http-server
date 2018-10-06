1fa768e6fb7e4adf50a8ff64137d5bc4d0577c45
Jerry <g.cggns@gmail.com>
2018-10-06 12:18:36 -0400

    v0.0.4.1 - Inline Documentation and Minor Fixes
    add: inline documentation
    fix: add text to status frame
    fix: remove redundant code

--- src/window/frames.py
+++ src/window/frames.py
@@ -14,29 +14,53 @@ except:
 		print("ERROR: No version of module tkinter is available")
 		quit()
 
+# This class must me initailized using a tk.Tk() object
+# The window this creates has 3 main frames
+#   Status Frame: This should contain status about the process the 
+#                 window is an interface for.
+#   Display Frame: This should contain active feedback from the process 
+#                  the window is an interface for.
+#   Command Frame: This collects input where it can be received by the 
+#                  process the window is an interface for.
+#
+# Functions intended to be called externally:
+#    setStatus(str)
+#    displayText(str)
+#    getCommand()
+#    clearDisplay()
+
 class Frames(tk.Frame):
+	# the maximum number of character that  can be displayed
+	# this is to prevent the buffer from taking all RAM when the program
+	#   is left runnign for long periods of time
 	DISPLAY_BUFFER_SIZE = 1024
 
-	def __init__(self, parent, title):
+	# parent (tk.Tk()) - the tk object to contain the frames
+	# title (str) - the text that will be displayed in the title bar
+	def __init__(self, parent, title="Window"):
+		# initialize the main window
+		# the name option allows for this class and its functions to be called
+		#   from outside the class
 		tk.Frame.__init__(self, parent, background="gray", name="main")
 		self.winfo_toplevel().title(title)
 
+		# configure the rows and columns for the main window
 		self.grid(sticky="nswe")
 		self.columnconfigure(0, weight=1)
 		self.rowconfigure(0, weight=0)
 		self.rowconfigure(1, weight=1)
 		self.rowconfigure(2, weight=0)
 
-		# status bar
+		#
+		# create and configure the status bar then add it to the main window
 		self.status = tk.Frame(self, background="red", borderwidth=4)
 		self.status.rowconfigure(0, weight=0)
 		self.status.columnconfigure(0, weight=1)
 		self.status.text = tk.Label(self.status, text="Status Bar", anchor="nw", justify="left")
-
-		# grid
 		self.status.text.grid(row=0, column=0, sticky="we")
 
-		# display window
+		#
+		# create and configure the display window then add it to the main window
 		self.display = tk.Frame(self, width=500, height=300, background="blue", borderwidth=4)
 		self.display.rowconfigure(0, weight=1)
 		self.display.columnconfigure(0, weight=1)
@@ -44,18 +68,37 @@ class Frames(tk.Frame):
 		self.display.grid(row=1, column=0, sticky="nswe")
 		self.display.grid_propagate(0)
 
+		# start the process of adding a scrollbar and scrolling region
+		# create a canvas as child of the frame that will parent the scrolling region
+		# the canvas controls what part of the area you see
 		self.display.canvas = tk.Canvas(self.display)
+		# create a frame that will be scrolled and set the canvas its parent
+		# the frame contains everything that can be shown
 		self.display.innerFrame = tk.Frame(self.display.canvas)
+		# create a scrollbar that will control the scrolling region
+		# NOTE: any frame can be the scrollbars parent but its reccommended to add 
+		#       it to the same frame the srolling region will be in
+		# OPTIONS: orient (str) - determine which direction the scrollbar will control
+		#          command () - set as the xview/yview of the canvas that is the scrollable area
 		self.display.scrollbar = tk.Scrollbar(self.display, orient="vertical", command=self.display.canvas.yview)
+		# associate the canvas of the scrolling area with the scrollbar
 		self.display.canvas.configure(yscrollcommand=self.display.scrollbar.set)
+		# start with the view at the top of the scrolling area
+		self.display.canvas.xview_moveto(0)
 
+		# add the canvas and scrollbar to the display window
 		self.display.canvas.grid(row=0, column=0, sticky="nswe")
 		self.display.scrollbar.grid(row=0, column=1, sticky="ns")
+		# create and get a refernce to a window inside the scrolling area canvas
+		# this actually creates the scrolling region in the canvas
+		# OPTION: window (tk.FRAME()) - the frame that will be displayed
 		innerFrameId = self.display.canvas.create_window(0, 0, window=self.display.innerFrame, anchor="nw")
+		# create some content to be shows in the scrolling frame
 		self.display.text = tk.Label(self.display.innerFrame, font="Helvetica 18 bold", text="Display Window", justify="left")
 		self.display.text.grid(row=0, column=0, sticky="nswe")
 
-		# command line
+		#
+		# create and configure the command bar then add it to the main window
 		self.command = tk.Frame(self, background="green", borderwidth=4)
 		self.command.rowconfigure(0, weight=0)
 		self.command.columnconfigure(1, weight=1)
@@ -64,9 +107,10 @@ class Frames(tk.Frame):
 		self.command.text.grid(row=0, column=0)
 		self.command.entry.grid(row=0, column=1, sticky="we")
 
+		# set focus to the command entry on startup
 		self.command.entry.focus()
 
-		# grid
+		# add the subframes to the main window
 		self.status.grid(row=0, column=0, sticky="we")
 		self.display.grid(row=1, column=0, sticky="nswe")
 		self.command.grid(row=2, column=0, sticky = "we")
@@ -75,21 +119,28 @@ class Frames(tk.Frame):
 		#    INLINE FUNCTIONS   #
 		# # # # # # # # # # # # #
 
+		# set the scrollable area size to the size of its content frame
 		def _updateScrollArea(self):
+			# get the size of the content
 			width = self.display.innerFrame.winfo_reqwidth()
 			height = self.display.innerFrame.winfo_reqheight()
 
+			# set the scroll area to the match the size of the content
 			self.display.canvas.config(scrollregion="0 0 {} {}".format(width, height))
-			self.display.canvas.xview_moveto(0)
+			#self.display.canvas.xview_moveto(0)
+
+			# make sure the view area is at the bottom of the resized scroll area
 			if(height > self.display.canvas.winfo_reqheight()):
 				self.display.canvas.yview_moveto(1)
 
+		# update scroll area on start
 		_updateScrollArea(self)
 
 		# # # # # # # # # # # #
 		#    EVENT BINDINGS   #
 		# # # # # # # # # # # #
 
+		# add the text in the command entry to the display window
 		# test function for developement
 		# in a project commands should be received by getCmmand() and 
 		# text should be added to the display frame with displayText()
@@ -99,29 +150,36 @@ class Frames(tk.Frame):
 
 		self.command.entry.bind_all("<Return>", _inputCommand)
 
+		# update the configuration of the scroll area's content 
 		# this runs on startup and whenever text is added to display.text
 		def _configure_displayText(event):
+			# get the size of the display canvas
 			width = self.display.canvas.winfo_width()
+			# set the wraplength of the display label
 			self.display.text.config(wraplength=width)
 
 		self.display.text.bind("<Configure>", _configure_displayText)
 
+		# update the configuration of the display canvas when its content changes size
 		# this runs on startup and whenever text is added to display.text
 		def _configure_interior(event):
+			# get the size of the content
 			width = self.display.innerFrame.winfo_reqwidth()
 			height = self.display.innerFrame.winfo_reqheight()
-			self.display.canvas.config(scrollregion="0 0 {} {}".format(width, height))
 
+			# if the size of the content has canged update the scroll area
 			if(self.display.innerFrame.winfo_reqwidth() != self.display.canvas.winfo_width()):
-				self.display.canvas.config(width=self.display.innerFrame.winfo_reqwidth())
 				_updateScrollArea(self)
 
+			# update the display contents to match the size change
 			_configure_displayText(event)
 
 		self.display.innerFrame.bind("<Configure>", _configure_interior)
 
+		# update the configuration of the display canvas when it changes size
 		# this runs on startup and when the main window is resized
 		def _configure_canvas(event):
+			# update the display window size in the canvas
 			if(self.display.innerFrame.winfo_reqwidth() != self.display.canvas.winfo_width()):
 				self.display.canvas.itemconfigure(innerFrameId, width=self.display.canvas.winfo_width())
 
@@ -131,9 +189,12 @@ class Frames(tk.Frame):
 	#   CLASS FUNCTIONS   #
 	# # # # # # # # # # # #
 
+	# set the text displayed in the status bar
 	def setStatus(self, info):
-		self.status.configure(text=info)
+		self.status.text["text"] = info
 
+	# get the text currently entered in the text entry then clear the netry
+	# RETURNS: (str) command
 	def getCommand(self):
 		cmd = self.command.entry.get()
 
@@ -141,28 +202,39 @@ class Frames(tk.Frame):
 		self.command.entry.delete(0, tk.END)
 		return cmd
 
+	# add text to the end the text currently stored in the display frame
+	# addText (str) - the text that will be added
+	# begin (str) - a string that gets placed at the beginning of addText
+	# end (str) - a string that gets placed at the end of addText
 	def displayText(self, addText, begin='\n >', end=''):
 		print("You Entered:", addText)
-		# remove newline from pressing enter in a command prompt
+
+		# get the old text and add the new text
 		currentText = self.display.text["text"]
 		currentText = currentText + begin + addText + end
 
 		# trim text if its too long
 		size = len(currentText)
 		if(size > self.DISPLAY_BUFFER_SIZE):
-			# find next new line and trim it and all before it
+			# find the place where the text must be trimmed to fit within the buffer
 			startSearchPos = size - self.DISPLAY_BUFFER_SIZE
+			# start searching for the next newline to determine where the text will be trimmed
 			trimIndex = currentText.find('\n', startSearchPos, size)
+
+			# check that a newline was found
 			if(trimIndex != -1):
 				# include the found '\n'
 				trimIndex = trimIndex + 1
-				currentText = currentText[(trimIndex):]
+				# trim the text
+				currentText = currentText[trimIndex:]
 			else:
 				print("frames.displayText() >> Single Entry Too Long")
 				currentText = ""
 
+		# apply the new text to the display text
 		self.display.text["text"] = currentText
 
+	# clears all text from the display window
 	def clearDisplay(self):
 		self.display.text["text"] = ""
 
@@ -177,6 +249,7 @@ if __name__ == "__main__":
 	# get a reference to the main Frames() object
 	# change the name="" in Frame().__init__() to use a different name
 	frame = root.nametowidget("main")
+	frame.setStatus("Status: running")
 	frame.displayText("Text From __main__()", begin="\n###  ", end="  ###")
 
 	root.mainloop()
